6e15fb531d3a7779e99ac23c18cc2c881abe42d3
rezardhy <rezardhiansyah19@gmail.com>
2021-06-01 00:51:04 +0700

    testing firestore

---
+++ app/src/main/java/com/reza/capstonecap0488/presentation/ui/SuggestionActivity.kt
@@ -0,0 +1,12 @@
+package com.reza.capstonecap0488.presentation.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.reza.capstonecap0488.R
+
+class SuggestionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_suggestion)
+    }
+}
--- app/src/main/java/com/reza/capstonecap0488/presentation/ui/result/ResultActivity.kt
+++ app/src/main/java/com/reza/capstonecap0488/presentation/ui/result/ResultActivity.kt
@@ -1,14 +1,17 @@
 package com.reza.capstonecap0488.presentation.ui.result
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.net.toUri
-import com.reza.capstonecap0488.R
+import com.google.firebase.firestore.FirebaseFirestore
 import com.reza.capstonecap0488.databinding.ActivityResultBinding
+
 
 class ResultActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityResultBinding
+    var db = FirebaseFirestore.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityResultBinding.inflate(layoutInflater)
@@ -18,6 +21,7 @@ class ResultActivity : AppCompatActivity() {
         actionBar?.setDisplayHomeAsUpEnabled(true)
         val img  = intent.getStringExtra("uri")
         binding.imgResult.setImageURI(img?.toUri())
+        cekFireStore()
 
     }
 
@@ -26,6 +30,23 @@ class ResultActivity : AppCompatActivity() {
         return true
     }
 
+
+    private fun cekFireStore(){
+        val docRef = db.collection("penyakit_tomat").document("Layu Bakteri")
+        docRef.get()
+            .addOnSuccessListener { document ->
+                if (document != null) {
+                    Log.d("berhasil", "Succes")
+
+                    binding.namaPenyakit.text = document.getString("nama")
+                } else {
+                    Log.d("kosong", "No such document")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.d("gagal", "get failed with ", exception)
+            }
+    }
 
     companion object{
         const val EXTRA = "uri"
