21da9e55bdb2ac55d4454920b71e7bc7fe632bac
rezardhy <rezardhiansyah19@gmail.com>
2021-06-01 15:45:01 +0700

    delete camera and gallery activity

--- app/src/main/java/com/reza/capstonecap0488/presentation/ui/searchpage/search/SearchFragment.kt
+++ app/src/main/java/com/reza/capstonecap0488/presentation/ui/searchpage/search/SearchFragment.kt
@@ -9,19 +9,15 @@ import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.reza.capstonecap0488.databinding.FragmentSearchBinding
-import com.reza.capstonecap0488.presentation.ui.searchpage.camera.CameraActivity
-import com.reza.capstonecap0488.presentation.ui.searchpage.gallery.GalleryActivity
 import com.reza.capstonecap0488.presentation.ui.searchpage.identification.IdentificationActivity
 import java.io.ByteArrayOutputStream
 
